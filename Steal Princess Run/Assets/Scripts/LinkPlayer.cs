9eba9580c5278f1353b83bb68779ef12e7e92710
klumzy33 <92058482+klumzy33@users.noreply.github.com>
2023-12-08 12:53:11 -0500

    Link colliding removed
    game doesn't end if link hits a box, it only ends when ganon hits a box

---
+++ Steal Princess Run/Assets/Scripts/LinkPlayer.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LinkPlayer : MonoBehaviour
+{
+    private bool horizontallyMoving = false;
+    private bool zero = true;
+    //   public bool isJumping = false;
+    //  public bool comingDown = false;
+    public GameObject playerObject;
+    public float startingSpeed = 20;
+    private float speed;
+    public float horizontalSpeed;
+    private CharacterController controller;
+    private Vector3 direction;
+    public ScoreManager scoreManager;
+
+    private int lane = 1;
+    public float laneDistance = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        speed = startingSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontalInput);
+        if (horizontalInput > 0 && zero)
+        {
+            zero = false;
+            lane++;
+            if (lane > 2)
+            {
+                lane = 2;
+            }
+            horizontallyMoving = true;
+        }
+        else if (horizontalInput < 0 && zero)
+        {
+            zero = false;
+
+            lane--;
+            if (lane < 0)
+            {
+                lane = 0;
+            }
+            horizontallyMoving = true;
+        }
+        else if (horizontalInput == 0)
+        {
+            zero = true;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 moveDirection = Vector3.zero;
+
+        if (horizontallyMoving)
+        {
+            Vector3 targetPosition = new Vector3(laneDistance * (lane - 1), currentPosition.y, currentPosition.z);
+            moveDirection = (targetPosition - currentPosition).normalized;
+            moveDirection.x = moveDirection.x * horizontalSpeed;
+
+            if ((targetPosition - currentPosition).sqrMagnitude < 0.01f)
+            {
+                horizontallyMoving = false;
+
+            }
+        }
+
+
+
+        // Apply constant movement along the z-axis
+        moveDirection.z = speed;
+
+        // Use CharacterController.Move for movement
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+
+        speed = Mathf.Floor(1.0f / 96.0f * currentPosition.z) + startingSpeed;
+    }
+
+   
+}
